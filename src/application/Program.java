89f9eafadbfa86b870070db8863b5787b79b146a
Dario Nakazima <dnakazima@hotmail.com>
2021-07-14 19:27:31 -0300

    inserindo dados a tabela seller retornando id

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -27,18 +29,37 @@ public class Program {
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentID) "
 					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)");	
+					+ "(?, ?, ?, ?, ?)",
+					//retornando o id gerado
+					Statement.RETURN_GENERATED_KEYS);	
 			
-			st.setString(1, "Dario");
-			st.setString(2, "dario@gmail.com");
+			st.setString(1, "Mario");
+			st.setString(2, "mario@gmail.com");
 			st.setDate(3, new java.sql.Date(sdf.parse("22/02/1985").getTime()));
-			st.setDouble(4, 3000.0);
+			st.setDouble(4, 6000.0);
 			st.setInt(5, 3);
 			
-			st.executeUpdate();
-			int rowsAffected = st.executeUpdate();
+			// retorna um int st.executeUpdate();
 			
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			int rowsAffected = st.executeUpdate();
+						
+			//System.out.println("Done! Rows affected: " + rowsAffected);
+			
+			if (rowsAffected > 0) {
+				//pegando o id  linha 33
+				st.getGeneratedKeys();
+				ResultSet rs = st.getGeneratedKeys();
+								
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			}
+			else {
+				System.out.println("No rows affected!");
+			}
+			
+			
 							
 		}
 		catch (SQLException e) {
