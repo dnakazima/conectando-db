d1a720aa21ea58729a33cd1dd6977cedd71eb095
Dario Nakazima <dnakazima@hotmail.com>
2021-07-14 17:40:57 -0300

    buscando dados no banco

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,33 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Connection conn = DB.getConnection();
-		DB.closeConection();
 
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+
+		try {
+			conn = DB.getConnection();
+
+			st = conn.createStatement();
+
+			rs = st.executeQuery("Select * from department");
+
+			while (rs.next()) {
+
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
