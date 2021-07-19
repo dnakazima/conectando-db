a435bae28decd4c0f0f894af662df72c5f02a698
Dario Nakazima <dnakazima@hotmail.com>
2021-07-19 09:44:01 -0300

    atualizando dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,78 +2,42 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		//inserindo um vendedor
-
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		Connection conn = null;
-		PreparedStatement st= null;
+		PreparedStatement st = null;
 		
 		try {
+		
 			conn = DB.getConnection();
-			
-			/*
-			st = conn.prepareStatement(
-					"INSERT INTO seller "
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentID) "
-					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)",
-					//retornando o id gerado
-					Statement.RETURN_GENERATED_KEYS);	
-			
-			st.setString(1, "Mario");
-			st.setString(2, "mario@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/02/1985").getTime()));
-			st.setDouble(4, 6000.0);
-			st.setInt(5, 3);
-			
-			*/
-			
-			// retorna um int st.executeUpdate();
-			
-			st = conn.prepareStatement("insert into department (Name) values ('D1'),('D2')", Statement.RETURN_GENERATED_KEYS);
-			
-			int rowsAffected = st.executeUpdate();
-						
-			//System.out.println("Done! Rows affected: " + rowsAffected);
-			
-			if (rowsAffected > 0) {
-				//pegando o id  linha 33
-				st.getGeneratedKeys();
-				ResultSet rs = st.getGeneratedKeys();
-								
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-				}
-			}
-			else {
-				System.out.println("No rows affected!");
-			}
-			
-			
-							
+		st = conn.prepareStatement(
+				"UPDATE seller "
+				+ "SET BaseSalary = BaseSalary + ? "
+				+ "WHERE "
+				+ "(DepartmentId = ?)");
+		
+		st.setDouble(1, 200);
+		st.setInt(2, 2);
+		
+		int rowsAffected = st.executeUpdate();
+		System.out.println("Done! Rows affected: " + rowsAffected);
+		
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
+			
 		}
-		
 		finally {
 			DB.closeStatement(st);
 			DB.closeConection();
+			
 		}
-	}
+	}	
 }
+
